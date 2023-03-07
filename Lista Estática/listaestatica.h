33c9ab23786c4cc675cc4737183a829d75edd1d6
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2023-03-07 01:03:29 -0300

    lista estática 2

--- Lista Estática/listaestatica.h
+++ Lista Estática/listaestatica.h
@@ -1,32 +1,35 @@
 #ifndef LISTAESTATICA_H_INCLUDED
 #define LISTAESTATICA_H_INCLUDED
+#include <iostream>
+
+using namespace std;
 
 struct ListaEstatica
 {
-  int n, elementos[];
+  int n, elementos[]; // onde n é a cardinalidade e elementos[] é o vetor
 };
 
 void criaLista(ListaEstatica& v)
 {
     v.n=0;
-};
+}
 
 bool verificaListaVazia(ListaEstatica v)
 {
     if(v.n==0) return true;
     else return false;
-};
+}
 
-bool verificaEspaço(ListaEstatica v)
+bool verificaEspaco(ListaEstatica v)
 {
     if(v.n<10) return true;
     else return false;
-};
+}
 
 int recuperaNumeroElementos(ListaEstatica v)
 {
     return v.n;
-};
+}
 
 bool verificaElemento(ListaEstatica v, int elem)
 {
@@ -34,14 +37,14 @@ bool verificaElemento(ListaEstatica v, int elem)
         if(v.elementos[i]==elem)
             return true;
     return false;
-};
+}
 
 bool verificaSePosicaoValida(ListaEstatica v, int pos)
 {
     if(pos>v.n)
         return false;
     return true;
-};
+}
 
 int recuperaElemento(ListaEstatica& v, int elem)
 {
@@ -49,7 +52,7 @@ int recuperaElemento(ListaEstatica& v, int elem)
         if(v.elementos[i]==elem)
             return elem;
     else return 0;
-};
+}
 
 int recuperaPosicaoElemento(ListaEstatica v, int elem)
 {
@@ -57,36 +60,52 @@ int recuperaPosicaoElemento(ListaEstatica v, int elem)
         if(v.elementos[i]==elem)
             return i;
     else return 0;
-};
+}
 
-void insereElementoInicio(ListaEstatica& v, int elem, int pos)
+void insereElemento(ListaEstatica& v, int elem, int pos)
+{
+    for(unsigned int i = 0; i < v.n; i++){
+        if(v.n==0){
+            v.elementos[v.n]=elem;
+            break;
+        }
+        if(i==pos){
+            for(unsigned int j = v.n - 1; j > i; j++){
+                v.elementos[j]=v.elementos[pos];
+            }
+            v.elementos[i]=elem;
+            break;
+        }
+        v.elementos[++i]=elem;
+    }
+    v.n++;
+}
+
+
+
+void excluiElemento(ListaEstatica& v, int elem, int pos)
 {
     for(unsigned int i = 0; i < v.n; i++){
         if(i==pos){
-            for(unsigned int j = v.n - 1; j > i; j++){
+            for(unsigned int j = i + 1; j < v.n; j++){
                 v.elementos[j]=v.elementos[i];
             }
             v.elementos[i]=elem;
         }
         break;
     }
-    v.n++;
-};
+    v.n--;
+}
 
-void insereElementoFinal(ListaEstatica& v, int elem)
-{
-    v.elementos[v.n++]=elem;
-};
-
-void exclueElemento(ListaEstatica& v)
+void excluiElemento(ListaEstatica& v)
 {
     v.elementos[v.n--];
-};
+}
 
 void mostraLista(ListaEstatica v)
 {
     for(unsigned int i = 0; i < v.n; i++)
-        cout << v.elementos[i];
-};
+        cout << "[" << v.elementos[i] << "]";
+}
 
 #endif // LISTAESTATICA_H_INCLUDED
--- Lista Estática/main.cpp
+++ Lista Estática/main.cpp
@@ -1,12 +1,25 @@
 #include "listaestatica.h"
 #include <iostream>
 
-
 using namespace std;
 
 int main()
 {
-    ListaEstatica v[10];
+    ListaEstatica lista;
+    int elem, pos;
+
+    criaLista(lista);
+    while(elem!=-1){
+        cout << "elemento: ";
+        cin >> elem;
+        if(elem==-1)
+            break;
+        cout << "posicao: ";
+        cin >> pos;
+        insereElemento(lista,elem,pos);
+    }
+    mostraLista(lista);
+
 
 
 
