031def974c450915cf32c77ff21db8d7cd64823e
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2023-03-09 00:02:22 -0300

    Add files via upload

--- Lista Estática/listaestatica.h
+++ Lista Estática/listaestatica.h
@@ -6,7 +6,7 @@ using namespace std;
 
 struct ListaEstatica
 {
-  int n, elementos[]; // onde n é a cardinalidade e elementos[] é o vetor
+  int n, elementos[10];
 };
 
 void criaLista(ListaEstatica& v)
@@ -41,71 +41,58 @@ bool verificaElemento(ListaEstatica v, int elem)
 
 bool verificaSePosicaoValida(ListaEstatica v, int pos)
 {
-    if(pos>v.n)
-        return false;
-    return true;
+    if(pos<10)
+        return true;
+    return false;
 }
 
-int recuperaElemento(ListaEstatica& v, int elem)
+int recuperaElemento(ListaEstatica& v, int pos)
 {
-    for(unsigned int i = 0; i < v.n; i++)
-        if(v.elementos[i]==elem)
-            return elem;
-    else return 0;
+    for(int i = 0; i < v.n; i++)
+        if(i==pos)
+            return v.elementos[pos];
+    else return -1;
 }
 
 int recuperaPosicaoElemento(ListaEstatica v, int elem)
 {
-    for(unsigned int i = 0; i < v.n; i++)
+    for(int i = 0; i < v.n; i++)
         if(v.elementos[i]==elem)
             return i;
-    else return 0;
+    else return -1;
 }
 
 void insereElemento(ListaEstatica& v, int elem, int pos)
 {
-    for(unsigned int i = 0; i < v.n; i++){
-        if(v.n==0){
-            v.elementos[v.n]=elem;
+    for(int i=0;i<v.n;i++){
+        if(pos==i){
+            for (int j=v.n; j>=pos; j--)
+                v.elementos[j]=v.elementos[j-1];
+            v.elementos[pos-1]=elem;
+            v.n++;
             break;
         }
-        if(i==pos){
-            for(unsigned int j = v.n - 1; j > i; j++){
-                v.elementos[j]=v.elementos[pos];
-            }
-            v.elementos[i]=elem;
-            break;
-        }
-        v.elementos[++i]=elem;
     }
-    v.n++;
 }
 
-
-
-void excluiElemento(ListaEstatica& v, int elem, int pos)
+void retiraElemento(ListaEstatica& v, int elem)
 {
-    for(unsigned int i = 0; i < v.n; i++){
-        if(i==pos){
-            for(unsigned int j = i + 1; j < v.n; j++){
-                v.elementos[j]=v.elementos[i];
+    for(int i=0; i<v.n; i++){
+        if(v.elementos[i]==elem){
+             for(int j=i;j<v.n;j++){
+                v.elementos[j]=v.elementos[++i];
+                v.n--;
+                break;
             }
-            v.elementos[i]=elem;
         }
         break;
     }
-    v.n--;
 }
 
-void excluiElemento(ListaEstatica& v)
+void exibeLista(ListaEstatica v)
 {
-    v.elementos[v.n--];
-}
-
-void mostraLista(ListaEstatica v)
-{
-    for(unsigned int i = 0; i < v.n; i++)
-        cout << "[" << v.elementos[i] << "]";
+    for(int i=0; i<v.n; i++)
+        cout << "[" << v.elementos[i] << "]" << endl;
 }
 
 #endif // LISTAESTATICA_H_INCLUDED
--- Lista Estática/main.cpp
+++ Lista Estática/main.cpp
@@ -3,25 +3,89 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
     ListaEstatica lista;
-    int elem, pos;
+    int respostaInt, elemento, posicao, varauxiliar;
+    bool respostaBool;
 
     criaLista(lista);
-    while(elem!=-1){
-        cout << "elemento: ";
-        cin >> elem;
-        if(elem==-1)
-            break;
-        cout << "posicao: ";
-        cin >> pos;
-        insereElemento(lista,elem,pos);
-    }
-    mostraLista(lista);
 
-
-
+    do{
+        cout << "\n0-Sair\n1-Verificar se esta vazia\n2-Verificar se ha espaço\n3-Recuperar numero de elementos\n";
+        cout << "4-Verificar determinado elemento\n5-Verificar se posicao e valida\n6-Recuperar elemento\n";
+        cout << "7-Recuperar posicao de elemento\n8-Inserir elemento\n9-Retirar elemento\n10-Exibir elementos\n";
+        cin >> respostaInt;
+        cin.ignore();
+        system("cls");
+        switch(respostaInt){
+            case 1:
+                respostaBool=verificaListaVazia(lista);
+                if(respostaBool)
+                    cout << "Lista vazia\n";
+                else
+                    cout << "Lista nao vazia\n";
+                break;
+            case 2:
+                respostaBool=verificaEspaco(lista);
+                if(respostaBool)
+                    cout << "Ha espaco\n";
+                else
+                    cout << "Nao ha espaco\n";
+                break;
+            case 3:
+                varauxiliar=recuperaNumeroElementos(lista);
+                cout << "Número de elementos na lista: " << varauxiliar << endl;
+                break;
+            case 4:
+                cout << "Informe o elemento: "; cin >> elemento;
+                respostaBool=verificaElemento(lista, elemento);
+                if(respostaBool)
+                    cout << "Esta na lista\n";
+                else
+                    cout << "Nao esta na lista\n";
+                break;
+            case 5:
+                cout << "Informe a posicao: "; cin >> posicao;
+                respostaBool=verificaSePosicaoValida(lista, posicao);
+                if(respostaBool)
+                    cout << "Posicao valida\n";
+                else
+                    cout << "Posicao invalida\n";
+                break;
+            case 6:
+                cout << "Informe a posicao: "; cin >> posicao;
+                respostaInt=recuperaElemento(lista, posicao);
+                if(respostaInt!=-1)
+                    cout << "Elemento: " << respostaInt << endl;
+                else
+                    cout << "Elemento inexistente\n";
+                break;
+            case 7:
+                cout << "Informe o elemento: "; cin >> elemento;
+                respostaInt=recuperaPosicaoElemento(lista, elemento);
+                if(respostaInt!=-1)
+                    cout << "Posicao: " << respostaInt << endl;
+                else
+                    cout << "Elemento nao encontrado\n";
+                break;
+            case 8:
+                cout << "Informe o elemento: "; cin >> elemento;
+                cout << "Informe sua posicao: "; cin >> posicao;
+                insereElemento(lista,elemento,posicao);
+                cout << "Elemento inserido\n";
+                break;
+            case 9:
+                cout << "Informe o elemento: "; cin >> elemento;
+                retiraElemento(lista, elemento);
+                cout << "Elemento retirado\n";
+                break;
+            case 10:
+                exibeLista(lista);
+                cout << endl;
+        }
+        system("pause");
+        system("cls");
+    }while(respostaInt!=0);
 
     return 0;
 }
